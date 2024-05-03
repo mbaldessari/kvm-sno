931d47a939aabf6bbae2970a8c3c509b00f2d61c
Michele Baldessari <michele@acksyn.org>
2024-05-03 08:35:00 +0200

    Shuffle things around

--- tests/full-testing.sh
+++ tests/full-testing.sh
@@ -23,21 +23,20 @@ echo "${START}: Start"
 echo "${START}: Start" > "${LOGDIR}/start.txt"
 
 TIME=$(date -Iminutes)
-echo "${TIME}: Install gitea in the background"
-make gitea-destroy gitea &> "${LOGDIR}/gitea-install.log" &
-
-# We kick off the setting up of the work SNOs in the background
-# That way they can chug along while we test gitops-iib etc
-TIME=$(date -Iminutes)
-echo "${TIME}: Install fresh SNOs"
-make EXTRA_VARS="-e vm_pool=fast" sno-destroy sno &> "${LOGDIR}/mcg-fresh.log"
-
-
-TIME=$(date -Iminutes)
 echo "${TIME}: Destroying and then installing test SNOs"
 make SNOS=sno10,sno11,sno12 sno-destroy sno &> "${LOGDIR}/mcg-test-snos.log"
 
 set +e
+# Let's do the ACM + MCE IIB dance here
+TIME=$(date -Iminutes)
+echo "${TIME}: Lookup gitops IIB"
+ansible-playbook -e "operator=acm" playbooks/iib-lookup.yml &> "${LOGDIR}/lookup-iib-acm.log"
+ansible-playbook -e "operator=multicluster-engine" playbooks/iib-lookup.yml &> "${LOGDIR}/lookup-iib-mce.log"
+TIME=$(date -Iminutes)
+echo "${TIME}: Install mcg via gitops IIB"
+make acm-iib EXTRA_VARS="-e iib_acm=$(cat /tmp/acm-iib) -e iib_mce=$(cat /tmp/multicluster-engine-iib) -e hub=sno10 -e spoke=sno11"
+ret_acm_iib=$?
+
 # This gets the latest IIB for gitops and writes it to /tmp/gitops-iib
 TIME=$(date -Iminutes)
 echo "${TIME}: Lookup gitops IIB"
@@ -47,22 +46,19 @@ echo "${TIME}: Install mcg via gitops IIB"
 make gitops-iib EXTRA_VARS="-e iib=$(cat /tmp/openshift-gitops-1-gitops-operator-bundle-iib) -e hub=sno12" &> "${LOGDIR}/gitops-iib-gitops.log"
 ret_gitops_iib=$?
 
-# Let's do the ACM + MCE IIB dance here
-TIME=$(date -Iminutes)
-echo "${TIME}: Lookup gitops IIB"
-ansible-playbook -e "operator=acm" playbooks/iib-lookup.yml &> "${LOGDIR}/lookup-iib-acm.log"
-ansible-playbook -e "operator=multicluster-engine" playbooks/iib-lookup.yml &> "${LOGDIR}/lookup-iib-mce.log"
-TIME=$(date -Iminutes)
-echo "${TIME}: Install mcg via gitops IIB"
-make acm-iib EXTRA_VARS="-e iib_acm=$(cat /tmp/acm-iib) -e iib_mce=$(cat /tmp/multicluster-engine-iib) -e gitrepo=https://github.com/mbaldessari/multicloud-gitops -e gitbranch=acm-iib-overrides -e hub=sno10 -e spoke=sno11"
-ret_acm_iib=$?
+echo "${TIME}: Everyting worked ok. Destroying test SNOs"
+make SNOS=sno10,sno11,sno12 sno-destroy &> "${LOGDIR}/mcg-destroy-after.log"
 
-# If both jobs were successfull we can just destroy the env if not we leave them running
-if [ $ret_acm_iib -eq 0 ] && [ $ret_gitops_iib ]; then
-    TIME=$(date -Iminutes)
-    echo "${TIME}: Everyting worked ok. Destroying test SNOs"
-    make SNOS=sno10,sno11,sno12 sno-destroy gitea-destroy &> "${LOGDIR}/mcg-destroy-after.log"
-fi
+TIME=$(date -Iminutes)
+echo "${TIME}: Install gitea in the background"
+make gitea-destroy gitea &> "${LOGDIR}/gitea-install.log" &
+
+# We kick off the setting up of the work SNOs in the background
+# That way they can chug along while we test gitops-iib etc
+TIME=$(date -Iminutes)
+echo "${TIME}: Install fresh SNOs"
+make EXTRA_VARS="-e vm_pool=fast" sno-destroy sno &> "${LOGDIR}/mcg-fresh.log"
+
 
 END=$(date -Iminutes)
 echo "${END}: End"
