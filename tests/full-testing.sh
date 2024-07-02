116fceb4b57d92ee30bcb6216d04929870b92617
Michele Baldessari <michele@acksyn.org>
2024-07-02 15:12:58 +0200

    Sort log files

--- tests/full-testing.sh
+++ tests/full-testing.sh
@@ -21,34 +21,34 @@ START=$(date -Iminutes)
 echo "${START}: Start"
 echo "${START}: Start" > "${LOGDIR}/start.txt"
 echo "${TIME}: Install fresh SNOs"
-make sno-destroy &> "${LOGDIR}/mcg-fresh-destroy.log"
-make sno &> "${LOGDIR}/mcg-fresh.log"
+make sno-destroy &> "${LOGDIR}/01-mcg-fresh-destroy.log"
+make sno &> "${LOGDIR}/02-mcg-fresh.log"
 
 set +e
 # Let's do the ACM + MCE IIB dance here
 TIME=$(date -Iminutes)
 echo "${TIME}: Lookup acm + mce IIB"
-ansible-playbook -e "operator=acm" -e "hub=sno5" playbooks/iib-lookup.yml &> "${LOGDIR}/lookup-iib-acm.log"
-ansible-playbook -e "operator=multicluster-engine" -e "hub=sno5" playbooks/iib-lookup.yml &> "${LOGDIR}/lookup-iib-mce.log"
+ansible-playbook -e "operator=acm" -e "hub=sno5" playbooks/iib-lookup.yml &> "${LOGDIR}/03-lookup-iib-acm.log"
+ansible-playbook -e "operator=multicluster-engine" -e "hub=sno5" playbooks/iib-lookup.yml &> "${LOGDIR}/04-lookup-iib-mce.log"
 TIME=$(date -Iminutes)
 echo "${TIME}: Install mcg via acm IIB"
-make acm-iib EXTRA_VARS="-e iib_acm=$(cat /tmp/acm-iib-sno5) -e iib_mce=$(cat /tmp/multicluster-engine-iib-sno5) -e hub=sno5 -e spoke=sno6" &> "${LOGDIR}/acm-iib-gitops.log"
+make acm-iib EXTRA_VARS="-e iib_acm=$(cat /tmp/acm-iib-sno5) -e iib_mce=$(cat /tmp/multicluster-engine-iib-sno5) -e hub=sno5 -e spoke=sno6" &> "${LOGDIR}/05-acm-iib-gitops.log"
 ret_acm_iib=$?
 
 # This gets the latest IIB for gitops and writes it to /tmp/gitops-iib
 TIME=$(date -Iminutes)
 echo "${TIME}: Lookup gitops IIB"
-ansible-playbook playbooks/iib-lookup.yml -e hub=sno3 &> "${LOGDIR}/lookup-iib.log"
+ansible-playbook playbooks/iib-lookup.yml -e hub=sno3 &> "${LOGDIR}/06-lookup-gitops-iib.log"
 TIME=$(date -Iminutes)
 echo "${TIME}: Install mcg via gitops IIB"
-make gitops-iib EXTRA_VARS="-e iib=$(cat /tmp/openshift-gitops-1-gitops-operator-bundle-iib-sno3) -e hub=sno3" &> "${LOGDIR}/gitops-iib-gitops.log"
+make gitops-iib EXTRA_VARS="-e iib=$(cat /tmp/openshift-gitops-1-gitops-operator-bundle-iib-sno3) -e hub=sno3" &> "${LOGDIR}/07-gitops-iib-gitops.log"
 ret_gitops_iib=$?
 
 # Both tests went fine
 if [ $ret_acm_iib -eq 0 ] && [ $ret_gitops_iib -eq 0 ]; then 
 	echo "${TIME}: Everyting worked ok. Destroying test SNOs"
-	make SNOS=sno3,sno4,sno5,sno6 sno-destroy &> "${LOGDIR}/mcg-destroy-after.log"
-	make SNOS=sno3,sno4,sno5,sno6 sno &> "${LOGDIR}/mcg-recreate-after.log"
+	make SNOS=sno3,sno4,sno5,sno6 sno-destroy &> "${LOGDIR}/08-mcg-destroy-after.log"
+	make SNOS=sno3,sno4,sno5,sno6 sno &> "${LOGDIR}/09-mcg-recreate-after.log"
 fi
 
 
