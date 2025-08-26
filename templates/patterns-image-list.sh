acadb9492de713cc434ae63d9df3ae99ccfc0b3d
Michele Baldessari <michele@acksyn.org>
2025-08-26 18:37:46 +0200

    more

---
+++ templates/patterns-image-list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+REPO=quay.io/hybridcloudpatterns
+CHARTS=(acm clustergroup gitea golang-external-secrets hashicorp-vault pattern-install)
+
+for i in "${CHARTS[@]}"; do
+	for j in $(skopeo list-tags docker://${REPO}/${i} | yq -r '.Tags[]'); do
+		echo "  - name: ${REPO}/${i}:${j}"
+	done
+done
