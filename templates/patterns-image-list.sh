a52e489870c48461ef09d3cb7f65092b6a2258e4
Michele Baldessari <michele@acksyn.org>
2026-03-28 12:08:27 +0100

    Add charts to mirroring

--- templates/patterns-image-list.sh
+++ templates/patterns-image-list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eu
 
-REPO=quay.io/hybridcloudpatterns
-CHARTS=(acm clustergroup gitea golang-external-secrets hashicorp-vault pattern-install)
+REPO=quay.io/validatedpatterns
+CHARTS=(acm clustergroup gitea golang-external-secrets openshift-external-secrets hashicorp-vault pattern-install)
 
 for i in "${CHARTS[@]}"; do
 	for j in $(skopeo list-tags docker://${REPO}/${i} | yq -r '.Tags[]'); do
